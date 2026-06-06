33444633d1512cd16da87b4681589fb7e2021f2f
JayCodes <ameenme@bugcrowdninja.com>
2026-06-06 20:54:49 +0100

    write interface for Hub

---
+++ src/Rebalancer.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.33;
+
+import {CCIPHelpers} from "./libraries/CCIPHelpers.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AllocationMaths} from "./libraries/AllocationMaths.sol";
+
+contract Rebalancer {}

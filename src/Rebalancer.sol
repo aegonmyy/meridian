ca9bb60c235c218151b405057b4b54260866931b
JayCodes <ameenme@bugcrowdninja.com>
2026-06-08 06:21:29 +0100

    finalized rebalancer initialized agentConsumer

--- src/Rebalancer.sol
+++ src/Rebalancer.sol
@@ -1,6 +1,7 @@
 // SPD,Licens,Identifier: MIT
 pragma solidity 0.8.33;
 
+import {CCIPHelpers} from "./libraries/CCIPHelpers.sol";
 import {AllocationMaths} from "./libraries/AllocationMaths.sol";
 import {IHub} from "./interfaces/IHub.sol";
 
@@ -49,19 +50,36 @@ contract Rebalancer {
     event ProtocolWhitelisted(bytes32 protocolId);
     event ProtocolRemovedFromWhitelist(bytes32 protocolId);
 
-    function proposeAllocation(AllocationProposal memory proposal) external onlyAuthorized {
+    constructor(address _hub, address _agentConsumer, address _owner) {
+        HUB = IHub(_hub);
+        AGENT_CONSUMER = _agentConsumer;
+        owner = _owner;
+    }
+
+    function proposeAllocation(
+        AllocationProposal memory proposal
+    ) external onlyAuthorized {
         if (block.timestamp - lastRebalanceTimestamp < COOLDOWN) {
             revert CooldownNotElapsed();
         }
-        bool valid = AllocationMaths.validateAllocation(proposal.proposedAllocations);
+        bool valid = AllocationMaths.validateAllocation(
+            proposal.proposedAllocations
+        );
         if (!valid) revert InvalidAllocation();
-        uint256 currentWeightedApy =
-            AllocationMaths.weightedApy(_flatten(proposal.currentAllocations), proposal.currentNetApys);
-        uint256 optimalWeightedApy =
-            AllocationMaths.weightedApy(_flatten(proposal.proposedAllocations), proposal.proposedNetApys);
-        bool rebalance = AllocationMaths.shouldRebalance(currentWeightedApy, optimalWeightedApy);
+        uint256 currentWeightedApy = AllocationMaths.weightedApy(
+            _flatten(proposal.currentAllocations),
+            proposal.currentNetApys
+        );
+        uint256 optimalWeightedApy = AllocationMaths.weightedApy(
+            _flatten(proposal.proposedAllocations),
+            proposal.proposedNetApys
+        );
+        bool rebalance = AllocationMaths.shouldRebalance(
+            currentWeightedApy,
+            optimalWeightedApy
+        );
 
-        if (!rebalance) revert();
+        if (!rebalance) revert BelowThreshold();
 
         for (uint256 i = 0; i < proposal.chainSelectors.length; i++) {
             if (whitelistedChains[proposal.chainSelectors[i]] == false) {
@@ -74,27 +92,58 @@ contract Rebalancer {
             }
         }
         uint256 totalAssets = HUB.totalAssets();
-        if (!AllocationMaths.validateSingleMove(proposal.proposedAllocations, totalAssets)) {
+        if (
+            !AllocationMaths.validateSingleMove(
+                proposal.proposedAllocations,
+                totalAssets
+            )
+        ) {
             revert MaxSingleMoveExceeded();
         }
+
         lastRebalanceTimestamp = block.timestamp;
+        for (uint256 i = 0; i < proposal.protocolIds.length; i++) {
+            CCIPHelpers.AdapterInstructions[]
+                memory _instructions = new CCIPHelpers.AdapterInstructions[](
+                    proposal.protocolIds.length
+                );
+            for (uint256 j = 0; i < proposal.protocolIds[i].length; j++) {
+                _instructions[i] = CCIPHelpers.AdapterInstructions({
+                    adapter: proposal.protocolIds[i][j],
+                    amount: proposal.proposedAllocations[i][j]
+                });
+            }
+            HUB.sendToSpoke(proposal.chainSelectors[i], _instructions);
+        }
     }
 
-    constructor(address _hub, address _agentConsumer, address _owner) {
-        HUB = IHub(_hub);
-        AGENT_CONSUMER = _agentConsumer;
-        owner = _owner;
+    function addChainToWhitelist(
+        uint64 _chainSelector
+    ) external onlyAuthorized {
+        whitelistedChains[_chainSelector] = true;
     }
 
-    function addChainToWhitelist(uint64 chainSelector) external {}
+    function removeChainFromWhitelist(
+        uint64 _chainSelector
+    ) external onlyAuthorized {
+        whitelistedChains[_chainSelector] = false;
+    }
 
-    function removeChainFromWhitelist(uint64 chainSelector) external {}
+    function addProtocolToWhitelist(
+        bytes32 _protocolId
+    ) external onlyAuthorized {
+        whitelistedProtocols[_protocolId] = true;
+    }
 
-    function addProtocolToWhitelist(bytes32 protocolId) external {}
+    function removeProtocolFromWhitelist(
+        bytes32 _protocolId
+    ) external onlyAuthorized {
+        whitelistedProtocols[_protocolId] = false;
+    }
 
-    function removeProtocolFromWhitelist(bytes32 protocolId) external {}
-
-    function _flatten(uint256[][] memory arr) internal pure returns (uint256[] memory) {
+    function _flatten(
+        uint256[][] memory arr
+    ) internal pure returns (uint256[] memory) {
         uint256 total;
         for (uint256 i = 0; i < arr.length; i++) {
             total += arr[i].length;
--- src/interfaces/IHub.sol
+++ src/interfaces/IHub.sol
@@ -2,20 +2,23 @@
 
 pragma solidity 0.8.33;
 
-struct AdapterInstructions {
-    bytes32 adapter;
-    uint256 amount;
-}
+import {CCIPHelpers} from "../libraries/CCIPHelpers.sol";
 
 contract IHub {
     function addSpoke(uint64 _chainSelector, address _spokeAddress) external {}
 
     function removeSpoke(uint64 _chainSelector) external {}
 
-    function sendToSpoke(uint64 _chainSelector, AdapterInstructions[] memory _instructions) external {}
+    function sendToSpoke(
+        uint64 _chainSelector,
+        CCIPHelpers.AdapterInstructions[] memory _instructions
+    ) external {}
 
-    function recallFromSpoke(uint64 _chainSelector, AdapterInstructions[] memory _instructions, bytes32 _messageId)
-        external {}
+    function recallFromSpoke(
+        uint64 _chainSelector,
+        CCIPHelpers.AdapterInstructions[] memory _instructions,
+        bytes32 _messageId
+    ) external {}
 
     function totalAssets() public view returns (uint256) {}
 }
